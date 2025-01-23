899298deefcb5ce02955d0d363098445419be079
Devangi Singh <singhdevangi@outlook.com>
2025-01-23 14:15:53 +0530

    Delete Q5.py

--- Q5.py
+++
@@ -1,9 +0,0 @@
-# PYTHON PROGRAM TO PRINT TABLE OF ANY NO.
-
-# user input
-num = int(input("Enter a number: "))
-
-# using for loop to iterate and calculate table of the number
-for i in range(1,11):
-    result = num * i
-    print(f"{num} x {i} = {result}")
